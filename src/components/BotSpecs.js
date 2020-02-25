e081a3a0e8382a264e1668402a1ad182f6b8aa67
campbelllssoup <matjocampbell@gmail.com>
2020-02-25 12:25:12 -0800

    Fixes commit issue.

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BotCard from "./BotCard"
 
 const BotSpecs = props => {
   let { bot } = props;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,39 @@
 import React from "react";
+import BotCollection from "./BotCollection"
+import YourBotArmy from "./YourBotArmy"
+
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+
+  state = {
+    bots: [],
+    botArmy: []
+  }
+
+  componentDidMount(){
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then( botsData => this.setState({bots: botsData},()=> console.log("this is bots data", botsData)))
+  }
+
+  // addToBotArmy = (bot) => {
+  // //  if(!this.state.botArmy.includes(bot)) {
+  // //    this.setState(prevState => ({
+  // //      ...prevState,
+  // //      botArmy: [...prevState.botArmy, bot]
+  // //    }))
+  // //  }
+  //   console.log("INSIDE add to BOT ARMY method")
+  // }
 
   render() {
     return (
       <div>
         {/* put your components here */}
+        
+        <BotCollection BotCollection={this.state.bots} />
+        {/* <YourBotArmy addToBotArmy={this.addToBotArmy}/> */}
       </div>
     );
   }
